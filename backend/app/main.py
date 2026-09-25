d01b9e8295c95238864d19bd16c975a0b0908a50
Pedro Freitas <phfreitas@ges.inatel.br>
2026-09-25 09:32:04 -0300

    fix: remove routes from `main.py` and remove `test_main.py`

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,26 +1,3 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
 
 app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return {"message": "Laboratório distribuído funcionando!"}
-
-
-@app.get("/is-even/{number}")
-def is_even(number: int):
-    if isinstance(number, str):
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-            detail="Não é possivel verificar paridade de string",
-        )
-
-    if isinstance(number, float):
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-            detail="Não é possivel verificar paridade de decimais",
-        )
-
-    result = number % 2 == 0
-    return {"result": result}
